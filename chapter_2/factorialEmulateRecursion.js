cb0fbb051cb8472954063ab013c6ca5851df048c
Simanta Barman <barma017@umn.edu>
2022-09-17 12:01:16 -0500

    fixed minor indentation

--- chapter_2/factorialEmulateRecursion.js
+++ chapter_2/factorialEmulateRecursion.js
@@ -4,23 +4,23 @@ callStack.push(currentDict);
 let returnValue;
 
 while (callStack.length > 0) {
-let number = callStack[callStack.length - 1]["number"]
-let returnAddress = callStack[callStack.length - 1]["returnAddress"]
+	let number = callStack[callStack.length - 1]["number"]
+	let returnAddress = callStack[callStack.length - 1]["returnAddress"]
 
-if (returnAddress === 'start') {
-    if (number === 1) {
-	returnValue = 1;
-	callStack.pop();
-	continue;
-    } else {
-	callStack[callStack.length - 1]["returnAddress"] = "after recursive call";
-	currentDict = {"returnAddress": "start", "number": number - 1};
-	callStack.push(currentDict);
-    }
-} else if (returnAddress === "after recursive call") {
-    returnValue = returnValue * number;
-    callStack.pop();
-    continue;
-}
+	if (returnAddress === 'start') {
+	    if (number === 1) {
+		returnValue = 1;
+		callStack.pop();
+		continue;
+	    } else {
+		callStack[callStack.length - 1]["returnAddress"] = "after recursive call";
+		currentDict = {"returnAddress": "start", "number": number - 1};
+		callStack.push(currentDict);
+	    }
+	} else if (returnAddress === "after recursive call") {
+	    returnValue = returnValue * number;
+	    callStack.pop();
+	    continue;
+	}
 }
 document.write(returnValue + "<br />");
